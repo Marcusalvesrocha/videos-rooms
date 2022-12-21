2bf43f6bb816c15f72c1ce946bdaa0d778c705a6
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-21 16:47:53 -0300

    adding models reviews, room and user

---
+++ app/models/room.rb
@@ -0,0 +1,8 @@
+class Room < ApplicationRecord
+  belongs_to :user
+  has_many :reviews
+
+  def avg_score
+    reviews.average(:score).round(2).to_f
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+userNew = User.create(name: "Marcus")
+
+rooms = Room.create([
+  {
+    title: "Sala 1",
+    user: userNew
+  },
+  {
+    title: "Sala 2",
+    user: userNew
+  },
+  {
+    title: "Sala 3",
+    user: userNew
+  },
+  {
+    title: "Sala 4",
+    user: userNew
+  },
+])
+
+reviews = Review.create([
+  {
+    title: "Nice room",
+    description: "I lovely the videos",
+    score: 5,
+    room: rooms.first
+  },
+  {
+    title: "is good",
+    description: "I hope is better",
+    score: 3,
+    room: rooms.first
+  }
+])
